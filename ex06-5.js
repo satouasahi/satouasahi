9af3103467e0ebd7aefc982acd238c229544b69b
satouasahi <zm19307@josai.ac.jp>
2021-02-01 13:54:12 +0000

    2/1

---
+++ ex06-5.js
@@ -0,0 +1,4 @@
+var who65 = 'world';
+function sayhello65() {alert('Hello, ' + who65 + '!');}
+function taro65() {who65 = '太郎';alert(who65 + 'が来た');}
+function hanako65() {who65 = '花子';alert(who65 + 'が来た');}
---
+++ ex6-9.js
@@ -0,0 +1,10 @@
+function txtchange1() {
+ document.getElementById('txt1').innerHTML= 'ナンバーワン！';
+}
+
+function imgchange1() {
+
+ document.getElementById('logo').src=
+ './image1/image2.png';
+}
+
